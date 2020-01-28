1942dcc3aa25216ea912d57cf83b0981c4803e23
Lucas-Werner <lucas.1werner@gmail.com>
2020-01-28 15:34:28 -0300

    greeting.spec.js

--- tests/greeting.spec.js
+++ tests/greeting.spec.js
@@ -1,5 +1,14 @@
 const greeting = require('../src/greeting');
 
+describe('#greeting', () => {
+  test('return message', () => {
+    expect(greeting('Lucas')).toEqual('Hello Lucas!');
+    expect(() => {
+      greeting();
+    }).toThrow(Error('You must send something to greet!'))
+  })
+})
+
 // A função deve receber uma string ${param} e retornar a mensagem 'Hello ${param}!'
 // Se nenhum parâmetro for passado um erro deve ser lançado
 // A mensagem do erro deve ser 'You must send something to greet!'
