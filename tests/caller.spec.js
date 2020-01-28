f45746585918e8b37a8c06dc3267f429be87ebbf
phtf92 <phtf92@gmail.com>
2020-01-28 17:37:58 -0300

    Code Climate

--- tests/caller.spec.js
+++ tests/caller.spec.js
@@ -1,8 +1,8 @@
 const caller = require('../src/caller');
 
-const funcaoMocada = jest.fn((a, b) => a + b)
+const funcaoMocada = jest.fn((a, b) => a + b);
 
 test('A função deve executar a função (primeiro parâmetro) com os outros parâmetros recebidos e retornar o resultado', () => {
-    expect(caller(funcaoMocada, 3, 4)).toBe(7);
-})
+  expect(caller(funcaoMocada, 3, 4)).toBe(7);
+});
 
