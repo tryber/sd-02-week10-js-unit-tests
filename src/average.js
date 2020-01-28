cddc3127a2048e32a281b0d3a02098645c763a14
Miguel Miranda <ayandram7727@gmail.com>
2020-01-28 16:45:05 -0200

    funções last e greeting prontas

--- src/average.js
+++ src/average.js
@@ -4,6 +4,8 @@
   Caso a função receba algum valor não número, o valor undefined deve ser retornado
 */
 
-function average(params) {}
+function average(...params) {
+  return params.reduce((acumulado, item) => acumulado + item, 0) / params.length  
+}
 
 module.exports = average;
--- tests/last.spec.js
+++ tests/last.spec.js
@@ -5,3 +5,9 @@ const last = require('../src/last');
 // Se a função receber uma lista, deve retornar o último elemento
 // Se a função receber uma string, deve retornar o último caractere
 // Se a função receber vários parâmetros, deve retornar o último parâmetro
+
+test('testando função last', () => {
+    expect(last(['a', 'h', 3, 'abelha'])).toBe('abelha')
+    expect(last('paralelepípedo')).toBe('o')
+    expect(last(1, 2, 'x', 'y', 'w')).toBe('w')
+})
