4a4508c1cb36a75e8ded402e365157d91e7291c3
EddyeBoy27 <lipe0503@gmail.com>
2020-01-28 17:33:44 -0200

    Finalizado Numbers

--- src/numbers.js
+++ src/numbers.js
@@ -3,6 +3,12 @@
   e retorna `true` se todos os parâmetros forem do tipo Number e `false` caso contrário
 */
 
-function numbers(params) {}
+function numbers(...params) {
+  const isNumber = params.every(param => typeof param === 'number');
+  if (isNumber) {
+    return true;
+  }
+  return false;
+}
 
 module.exports = numbers;
