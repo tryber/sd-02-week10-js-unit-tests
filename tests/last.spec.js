f00d66e94a4400f9c8e54c71494a6063dfdf7c10
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-01-28 17:09:10 -0300

    Requisito 1

--- tests/last.spec.js
+++ tests/last.spec.js
@@ -5,3 +5,17 @@ const last = require('../src/last');
 // Se a função receber uma lista, deve retornar o último elemento
 // Se a função receber uma string, deve retornar o último caractere
 // Se a função receber vários parâmetros, deve retornar o último parâmetro
+
+describe('testing the last of', () => {
+  test('receiving array and returning last element', () => {
+    expect(last([1, 4, 16, 64])).toBe(64);
+  });
+
+  test('receiving string and returning last character', () => {
+    expect(last('Trybe')).toBe('e');
+  });
+
+  test('receiving more than one parameter and returning last parameter', () => {
+    expect(last('mouse', 'teclado', 435)).toBe(435);
+  });
+});
