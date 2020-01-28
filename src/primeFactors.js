4d70e594006efee39dcc2b1cc46d459e1c128dd6
Douglas Henrique <doughrqe@gmail.com>
2020-01-28 16:10:17 -0300

    primeFactors e spec

--- src/primeFactors.js
+++ src/primeFactors.js
@@ -3,6 +3,17 @@
   e retorna uma lista com todos os fatores desse parâmetro
 */
 
-function primeFactors(number) {}
+function primeFactors(number) {
+  if (Number.isInteger(number)) {
+    const arr = [];
+    for (let i = 0; i <= number; i += 1) {
+      if (number % i === 0) {
+        arr.push(i);
+      }
+    }
+    return arr;
+  }
+  throw new Error('Não é um número');
+}
 
 module.exports = primeFactors;
--- tests/primeFactors.spec.js
+++ tests/primeFactors.spec.js
@@ -3,3 +3,5 @@ const assert = require('assert');
 
 // A função recebe um inteiro e retorna a lista com os fatores desse número
 // Se o parâmetro recebido for um valor não inteiro um erro deve ser lançado
+assert.deepStrictEqual(primeFactors(10), [1, 2, 5, 10])
+assert.throws(() => { primeFactors('Abacate') });
