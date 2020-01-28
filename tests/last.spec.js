7451726a107949ab7ae8222228ab685e3452a4ab
lauro <laurolyra@gmail.com>
2020-01-28 15:48:47 -0300

    testes em last concluídos

--- tests/last.spec.js
+++ tests/last.spec.js
@@ -1,5 +1,20 @@
 const last = require('../src/last');
 
+describe('série de testes para last.js', () => {
+  it('primeiro teste: array', () => {
+    expect(last(['gato', 'cachorro', 'papagaio'])).toBe('papagaio');
+  }
+  )
+  it('segundo teste: string', () => {
+    expect(last('cachorro')).toBe('o')
+  }
+  )
+  it('terceiro e último teste: vários parâmetros', () => {
+    expect(last(1, 2, 'vassoura', harryPotter = 0)).toBe(0)
+  })
+});
+
+
 // A função deve receber uma quantidade qualquer de parâmetros
 // A função deve retornar o último elemento do parâmetro passado
 // Se a função receber uma lista, deve retornar o último elemento
