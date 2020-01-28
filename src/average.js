3fda34cdcb20ba82a20f369ca224fe3afabf8dbc
Lucas-Werner <lucas.1werner@gmail.com>
2020-01-28 14:58:06 -0300

    função average

--- src/average.js
+++ src/average.js
@@ -4,6 +4,12 @@
   Caso a função receba algum valor não número, o valor undefined deve ser retornado
 */
 
-function average(params) {}
+function average(...params) {
+  const total = params.reduce((acc, cur) => acc + cur)
+  const media = total / params.length
+  console.log(media)
+}
+
+average(9, 1, 5)
 
 module.exports = average;
