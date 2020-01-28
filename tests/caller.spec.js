eaa319084ddb68804c46bad460086219184343a4
Ishidor <kevinmendessilva@hotmail.com>
2020-01-28 18:21:44 -0300

    step 8 done

--- tests/caller.spec.js
+++ tests/caller.spec.js
@@ -5,3 +5,14 @@ const caller = require('../src/caller');
 // O segundo e terceiro parâmetros podem ser de qualquer tipo
 // A função deve executar a função (primeiro parâmetro) com os outros parâmetros recebidos
 // e retornar o resultado
+
+const fn = (a, b) => a + b;
+
+describe('#caller', () => {
+  it('with fn, 30, 70 should return 100', () => {
+    expect(caller(fn, 30, 70)).toBe(100);
+  });
+  it('should return 2 strings as 1', () => {
+    expect(caller(fn, 'Hello ', 'Tryber')).toBe('Hello Tryber');
+  });
+});
