522a400dcb984f7803a444f1e5db5cc4dd261477
Lucas-Werner <lucas.1werner@gmail.com>
2020-01-28 19:03:44 -0300

    caller

--- tests/caller.spec.js
+++ tests/caller.spec.js
@@ -1,5 +1,22 @@
 const caller = require('../src/caller');
 
+const soma = jest.fn((a, b) => a + b);
+
+describe('#caller', () => {
+  it('add numbers', () => {
+    expect(caller(soma, 3, 3)).toBe(6);
+    expect(caller(soma, 4, 2)).toBe(6);
+    expect(soma).toBeCalledWith(3, 3);
+    expect(soma).toBeCalledWith(4, 2);
+    expect(soma).toHaveBeenCalledTimes(2);
+  }
+  )
+  it('concat strings', () => {
+    expect(caller(soma, 'lucas', 'werner')).toBe('lucaswerner');
+    expect(soma).toHaveBeenCalledTimes(1);
+  })
+})
+
 // A função deve receber 3 parâmetros
 // O primeiro parâmetro deve ser uma função
 // O segundo e terceiro parâmetros podem ser de qualquer tipo
