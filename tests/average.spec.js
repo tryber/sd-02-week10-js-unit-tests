2a557f092068366bc9e83f4199b3b0b26b7e1ea6
Ishidor <kevinmendessilva@hotmail.com>
2020-01-28 18:08:44 -0300

    step 6 & 7 done

--- tests/average.spec.js
+++ tests/average.spec.js
@@ -8,4 +8,13 @@ describe('#average', () => {
   it('expect average of 3, 4, 5 should be 4', () => {
     expect(average(3, 4, 5)).toBe(4);
   });
+  it('expect average of 50, 50, 50 should be 50', () => {
+    expect(average(50, 50, 50)).toBe(50);
+  });
+  it('expect average of 3, 4 and a string should be undefined', () => {
+    expect(average(3, 4, '5')).toBe(undefined);
+  });
+  it('expect average of two strings should be undefined', () => {
+    expect(average('this', 'that')).toBe(undefined);
+  });
 });
