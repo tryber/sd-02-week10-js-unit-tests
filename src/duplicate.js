d92f3dbeacb1773d74b9eac268f16db441da626f
lauro <laurolyra@gmail.com>
2020-01-28 17:41:45 -0300

    duplicate.js pronta

--- src/duplicate.js
+++ src/duplicate.js
@@ -3,6 +3,10 @@
   e retorna `true` se existe algum elemento duplicado e `false` caso contrário
  */
 
-function duplicate(params) {}
+function duplicate(params) {
+  const arrayParam = Array.from(arguments)
+  const repetidos = arrayParam.filter((valor, indice) => arrayParam.indexOf(valor) === indice);
+  return arrayParam.length != repetidos.length
+}
 
 module.exports = duplicate;
