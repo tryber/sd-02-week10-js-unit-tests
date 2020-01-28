cddca38b1ab56a642d3f91dbd3733e9e2da155d7
lauro <laurolyra@gmail.com>
2020-01-28 18:01:35 -0300

    average feito pela metade

--- src/average.js
+++ src/average.js
@@ -4,6 +4,11 @@
   Caso a função receba algum valor não número, o valor undefined deve ser retornado
 */
 
-function average(params) {}
+function average(params) {
+  if(typeof arguments != 'number') {
+    return undefined
+  }
+  return (Array.from(arguments).reduce((a,b) => a+b,0))/3
+}
 
 module.exports = average;
