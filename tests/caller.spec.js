3da7240b6dc7e0ea024d786ce280ad378468d9e2
Fernando Luckesi <fernando.luckesi@gmail.com>
2020-01-28 18:32:31 -0300

    Requisito 8  finalizado

--- tests/caller.spec.js
+++ tests/caller.spec.js
@@ -5,3 +5,16 @@ const caller = require('../src/caller');
 // O segundo e terceiro parâmetros podem ser de qualquer tipo
 // A função deve executar a função (primeiro parâmetro) com os outros parâmetros recebidos
 // e retornar o resultado
+
+
+test('#caller', () => {
+  function sum (...params) {
+        return params.reduce((cc, current) => cc + current);
+  }
+
+  function mult (...params) {
+    return params.reduce((cc, current) => cc * current);
+  }
+  expect(caller(sum, 2, 5)).toBe(7);
+  expect(caller(mult, 10, 10)).toBe(100);
+});
