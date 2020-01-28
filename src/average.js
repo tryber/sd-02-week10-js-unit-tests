932c71bcf0cf60674a58cf26df67a0bb442d7cd2
guicgs <etohlif@protonmail.com>
2020-01-28 18:12:25 -0300

    função average concluída

--- src/average.js
+++ src/average.js
@@ -3,14 +3,13 @@
   e retorna a média dos valores recebidos
   Caso a função receba algum valor não número, o valor undefined deve ser retornado
 */
-const numbers = require('./numbers');
-
+// const numbers = require('./numbers');
 function average(...params) {
   const elementsAreNumber = params.every(element => typeof element === 'number');
   if (elementsAreNumber)
     return params.reduce((acc, cur) => acc + cur) / params.length;
-  // if (!numbers(params)) return undefined;  
-  // return params.reduce(((acc, cur) => acc + cur) / params.length);
+  // if (numbers(...params)) return [...params].reduce(((acc, cur) => acc + cur) / params.length);
+  // return undefined;  
 }
 
 module.exports = average;
