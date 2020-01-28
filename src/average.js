34af9327008ba9a91981028c2a418a50f4c55671
EddyeBoy27 <lipe0503@gmail.com>
2020-01-28 14:26:12 -0200

    Iniciando Projeto, Segura Na Mao de Deus e Vai

--- src/average.js
+++ src/average.js
@@ -4,6 +4,8 @@
   Caso a função receba algum valor não número, o valor undefined deve ser retornado
 */
 
-function average(params) {}
+function average(params) {
+  
+}
 
 module.exports = average;
