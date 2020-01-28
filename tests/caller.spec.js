3909b65eb91b5d5b4e4153451b41f7326547d343
Douglas Henrique <doughrqe@gmail.com>
2020-01-28 02:20:24 -0300

    caller spec

--- tests/caller.spec.js
+++ tests/caller.spec.js
@@ -5,3 +5,13 @@ const caller = require('../src/caller');
 // O segundo e terceiro parâmetros podem ser de qualquer tipo
 // A função deve executar a função (primeiro parâmetro) com os outros parâmetros recebidos
 // e retornar o resultado
+
+describe('#Caller', () => {
+    const some = jest.fn((a, b) => a + b);
+    const div = jest.fn((a, b) => b / a);
+    it('Teste de função', done => {
+        expect(caller(some, 1, 2)).toBe(3);
+        expect(caller(div, 1, 2)).toBe(2);
+        done();
+    })
+})
