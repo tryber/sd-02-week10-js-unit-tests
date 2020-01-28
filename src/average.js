f2a8727fa3b877bb2d6efda8409daeb9c38c6693
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2020-01-28 15:38:41 -0200

    Finalizado a funcao average e os testes

--- src/average.js
+++ src/average.js
@@ -3,7 +3,14 @@
   e retorna a média dos valores recebidos
   Caso a função receba algum valor não número, o valor undefined deve ser retornado
 */
-
-function average(params) {}
+function average(...params) {
+  const isNumber = !params.some(el => !(typeof el === 'number'));
+  if (isNumber && (params.length !== 0)) {
+    return (params.reduce((total, value) => {
+      return (total + value);
+    },0)/params.length);
+  }
+  return undefined;
+}
 
 module.exports = average;
