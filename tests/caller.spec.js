dc029385aaf7577bcbfaf02190b60704ff8315cc
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-01-28 19:30:54 -0300

    CodeClimate - funcao caller

--- tests/caller.spec.js
+++ tests/caller.spec.js
@@ -8,29 +8,30 @@ const caller = require('../src/caller');
 
 const mockCallback = jest.fn((a, b) => a + b);
 
-describe('testing caller function', () => {
-    
-    afterEach(() => {    
-        jest.clearAllMocks();
-    });
-    
-    test('is a function', () => {
-        expect(typeof mockCallback).toBe('function');
-    });
+describe('testing caller function', () => { 
+  afterEach(() => {    
+    jest.clearAllMocks();
+  }); 
+  
+  test('is a function', () => {
+    expect(typeof mockCallback).toBe('function');
+  });
 
-    test('testing with numbers', () => {
-        expect(caller(mockCallback, 10, 20)).toBe(30);
-        expect(mockCallback).toHaveBeenCalledWith(10, 20);
-        expect(mockCallback).toHaveBeenCalledTimes(1);
-    })
-    test('testing with strings', () => {
-        expect(caller(mockCallback, 'projeto ', '10')).toBe('projeto 10');
-        expect(mockCallback).toHaveBeenCalledWith('projeto ', '10');
-        expect(mockCallback).toHaveBeenCalledTimes(1);
-    })
-    test('testing with arrays', () => {
-        expect(caller(mockCallback, ['projeto'], [' npm', 'test'])).toBe('projeto npm,test');
-        expect(mockCallback).toHaveBeenCalledWith(['projeto'], [' npm', 'test']);
-        expect(mockCallback).toHaveBeenCalledTimes(1);
-    })
-})
+  test('testing with numbers', () => {
+    expect(caller(mockCallback, 10, 20)).toBe(30);
+    expect(mockCallback).toHaveBeenCalledWith(10, 20);
+    expect(mockCallback).toHaveBeenCalledTimes(1);
+  });
+
+  test('testing with strings', () => {
+    expect(caller(mockCallback, 'projeto ', '10')).toBe('projeto 10');
+    expect(mockCallback).toHaveBeenCalledWith('projeto ', '10');
+    expect(mockCallback).toHaveBeenCalledTimes(1);
+  });
+
+  test('testing with arrays', () => {
+    expect(caller(mockCallback, ['projeto'], [' npm', 'test'])).toBe('projeto npm,test');
+    expect(mockCallback).toHaveBeenCalledWith(['projeto'], [' npm', 'test']);
+    expect(mockCallback).toHaveBeenCalledTimes(1);
+  });
+});
