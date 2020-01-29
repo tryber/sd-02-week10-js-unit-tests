2ded0deb8fede03964a9ccea4919173023c0676d
Lucas-Werner <lucas.1werner@gmail.com>
2020-01-29 19:32:52 -0300

    ok

--- tests/caller.spec.js
+++ tests/caller.spec.js
@@ -3,17 +3,19 @@ const caller = require('../src/caller');
 const soma = jest.fn((a, b) => a + b);
 
 describe('#caller', () => {
-  it('add numbers', () => {
+  it('add numbers', done => {
     expect(caller(soma, 3, 3)).toBe(6);
     expect(caller(soma, 4, 2)).toBe(6);
     expect(soma).toBeCalledWith(3, 3);
     expect(soma).toBeCalledWith(4, 2);
     expect(soma).toHaveBeenCalledTimes(2);
+    done()
   }
   )
-  it('concat strings', () => {
+  it('concat strings', done => {
     expect(caller(soma, 'lucas', 'werner')).toBe('lucaswerner');
     expect(soma).toHaveBeenCalledTimes(1);
+    done();
   })
 })
 
