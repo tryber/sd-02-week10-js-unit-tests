b1cf790293d88fb289e1c5b0992e7abe463c86c9
EddyeBoy27 <lipe0503@gmail.com>
2020-01-29 03:16:15 -0200

    Finalizado primeFactors

--- src/primeFactors.js
+++ src/primeFactors.js
@@ -3,6 +3,20 @@
   e retorna uma lista com todos os fatores desse parâmetro
 */
 
-function primeFactors(number) {}
+function primeFactors(number) {
+  if (Number.isInteger(number)) {
+    let lista = [];
+    let numAtual = number;
+    for (i = 2; i <= numAtual; i += 1) {
+      if (numAtual % i == 0) {
+        lista.push(i);
+        numAtual = numAtual / i;
+        i -= 1;
+      }
+    }
+    return lista;
+  }
+  return (`${number} is not a number`);
+}
 
 module.exports = primeFactors;
--- tests/primeFactors.spec.js
+++ tests/primeFactors.spec.js
@@ -1,5 +1,18 @@
 const primeFactors = require('../src/primeFactors');
-const assert = require('assert');
+//const assert = require('assert');
 
 // A função recebe um inteiro e retorna a lista com os fatores desse número
 // Se o parâmetro recebido for um valor não inteiro um erro deve ser lançado
+
+//const expect = primeFactors(112);
+
+//assert.deepStrictEqual(expect, [ 2, 2, 2, 2, 7 ], 'Deu ruim, tenta de novo');
+
+describe('#primeFactors', () => {
+  it('expect factoring 112 [ 2, 2, 2, 2, 7 ]', () => {
+    expect(primeFactors(112)).toEqual([ 2, 2, 2, 2, 7 ]);
+  });
+  it('expect error message if dont send number param', () => {
+    expect(primeFactors(3.12)).toEqual(`3.12 is not a number`);
+  });
+});
