45dfa14f5b718e5e7e517419fd7bb5ac930bf013
Lucas-Werner <lucas.1werner@gmail.com>
2020-01-29 14:01:02 -0300

    antes do pull

--- src/primeFactors.js
+++ src/primeFactors.js
@@ -3,6 +3,24 @@
   e retorna uma lista com todos os fatores desse parâmetro
 */
 
-function primeFactors(number) {}
+// function primeFactors(number) {
+//   if(number.isInteger(number))
+  
+// }
 
-module.exports = primeFactors;
+// module.exports = primeFactors;
+function primeFactors(number) {
+  if (!Number.isInteger(number)) throw new Error('You must send an integer number!');
+  const factors = [];
+  for (let i = 2; i <= number; i += 1) {
+    if (number % i === 0) {
+    factors.push(i);
+    number /= i;
+  }
+}
+  return factors.filter((num, index, arr) => index === arr.indexOf(num))
+
+  
+}
+
+console.log(primeFactors(0))
--- tests/primeFactors.spec.js
+++ tests/primeFactors.spec.js
@@ -1,5 +1,17 @@
 const primeFactors = require('../src/primeFactors');
 const assert = require('assert');
 
+describe('#primefactors', () => {
+  it('integer return factors', () => {
+    assert.deepStrictEqual(primeFactors(10), [2, 5]);
+    assert.deepStrictEqual(primeFactors(147), [2, 5, 13]);
+    assert.deepStrictEqual(primeFactors(0), [3, 5])
+  })
+  it('not a integer', () => {
+    assert.throws(() => primeFactors(2.3), Error('You must pass a integer')
+  })
+  
+})
+
 // A função recebe um inteiro e retorna a lista com os fatores desse número
 // Se o parâmetro recebido for um valor não inteiro um erro deve ser lançado
