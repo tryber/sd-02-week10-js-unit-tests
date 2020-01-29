0ae5b9ec381a2a60b0c15601935b3725738149f3
phtf92 <phtf92@gmail.com>
2020-01-29 00:06:42 -0300

    printBinary

--- src/printBinary.js
+++ src/printBinary.js
@@ -4,11 +4,38 @@
  */
 
 function printBinary(number) {
+  const respostaEmColunas = [];
+  if (!number) return respostaEmColunas;
+  const nNumerosBi = 2 ** number;
+  for (let i = 1; i <= number; i += 1) {
+    respostaEmColunas.push(criarColuna(nNumerosBi, nNumerosBi / 2 ** i))
+  }
   const resposta = [];
-  if (!number) return resposta;
-  const nColunas = number;
-  const linhas = 2**number;
+  for (let i = 0; i < nNumerosBi; i++) {
+    let formarLinha = '';
+    respostaEmColunas.forEach(Element => {
+      formarLinha += Element[i];
+    })
+    resposta.push(formarLinha)
+  }
+  console.log(resposta)
 }
 
+function criarColuna(comprimento, intervalo) {
+  let coluna = '';
+  for (let i = 1; i <= comprimento; i += 0) {
+    for (let j = 0; j < intervalo; j += 1) {
+      coluna += '0'.repeat(1)
+      i += 1;
+    }
+    for (let j = 0; j < intervalo; j += 1) {
+      coluna += '1'.repeat(1)
+      i += 1;
+    }
+  }
+  return coluna
+}
+
+printBinary(3)
 
 module.exports = printBinary;
