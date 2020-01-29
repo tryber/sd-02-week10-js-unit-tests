0f1aead7e81f8b148b338ca2aab843c69dcd4965
phtf92 <phtf92@gmail.com>
2020-01-28 23:05:26 -0300

    caller

--- tests/caller.spec.js
+++ tests/caller.spec.js
@@ -6,3 +6,10 @@ test('A função deve executar a função (primeiro parâmetro) com os outros pa
   expect(caller(funcaoMocada, 3, 4)).toBe(7);
 });
 
+test('A função mock foi chamada', () => {
+  expect(funcaoMocada).toHaveBeenCalled();
+});
+
+test('Conferindo parâmetros passados', () => {
+  expect(funcaoMocada).toHaveBeenLastCalledWith(3, 4)
+});
