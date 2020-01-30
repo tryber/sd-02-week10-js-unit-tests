44392f27acecc98425871d323cbf78f941389998
Miguel Miranda <ayandram7727@gmail.com>
2020-01-30 02:10:45 -0200

    função printBinary com testes

--- src/printBinary.js
+++ src/printBinary.js
@@ -4,21 +4,22 @@
  */
 
 function printBinary(number) {
+  if (number === 0 || typeof number !== 'number') {
+    return [];
+  }
+
   const lista = ['0', '1'];
 
   for (let i = 1; i < number; i += 1) {
-    let tamanhoLista = lista.length;
+    const tamanhoLista = lista.length;
 
     for (let j = 0; j < tamanhoLista; j += 1) {
       lista.push(`${lista[j]}0`);
       lista.push(`${lista[j]}1`);
     }
   }
-  console.log(lista)
 
   return lista.slice(lista.length - 2 ** number);
 }
-
-console.log(printBinary(2))
 
 module.exports = printBinary;
