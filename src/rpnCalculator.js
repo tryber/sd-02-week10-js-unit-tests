446a65cba263416113c55b0f25e6be132445ba8b
Douglas Henrique <doughrqe@gmail.com>
2020-01-30 02:17:49 -0300

    Função rpn

--- src/rpnCalculator.js
+++ src/rpnCalculator.js
@@ -5,6 +5,17 @@
   referência: https://en.wikipedia.org/wiki/Reverse_Polish_notation
  */
 
-function rpnCalculator(expression) {}
+function rpnCalculator(string) {
+  const arr = [];
+  string.split(' ').forEach(Element => {
+    if (Element == +Element) {
+      arr.push(Element);
+    } else {
+      arr.push(eval(arr.splice(-2, 1)[0] + Element + arr.pop()));
+    }
+  });
+  return arr[0];
+}
 
+console.log(rpnCalculator('15 7 1 1 + - / 3 * 2 1 1 + + -'))
 module.exports = rpnCalculator;
