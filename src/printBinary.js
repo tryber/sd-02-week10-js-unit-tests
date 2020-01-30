ac03f93025f3b7324cebdc051c0248352b356623
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-01-30 02:20:54 -0300

    Requisito 9 e 10

--- src/printBinary.js
+++ src/printBinary.js
@@ -3,6 +3,18 @@
   e retorna uma lista com todos os números binários existentes com a quantidade ${number} de dígitos
  */
 
-function printBinary(number) {}
-
+function printBinary(number) {
+  let arrResp = [];
+  if (typeof number !== 'number' || number <= 0) return arrResp;
+  maxValue = (2 ** number);
+  for (let i = 0; i < maxValue; i += 1){
+    arrResp.push(i.toString(2));
+  };
+  for (let i = 0; i < arrResp.length; i +=1){
+    if(arrResp[i].length < number){
+      arrResp[i] = '0'.repeat(number - arrResp[i].length) + arrResp[i];
+    };
+  };
+  return arrResp;
+};
 module.exports = printBinary;
