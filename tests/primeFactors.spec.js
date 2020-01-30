75c40641b92c28e25086d424807113ab63c27476
Ishidor <kevinmendessilva@hotmail.com>
2020-01-30 13:36:57 -0300

    CC fix

--- tests/primeFactors.spec.js
+++ tests/primeFactors.spec.js
@@ -6,15 +6,15 @@ const assert = require('assert');
 
 describe('primeFactors tests', () => {
   test('prime factors of 15 should be 3 & 5', () => {
-      assert.deepEqual(primeFactors(15), [3, 5]);
+    assert.deepEqual(primeFactors(15), [3, 5]);
   });
   test('prime factors of 9 should be 3 & 3', () => {
-      assert.deepEqual(primeFactors(9), [3, 3]);
+    assert.deepEqual(primeFactors(9), [3, 3]);
   });
   test('prime factors of 12 should be 2, 2 & 3', () => {
-      assert.deepEqual(primeFactors(12), [2, 2, 3]);
+    assert.deepEqual(primeFactors(12), [2, 2, 3]);
   });
   test('if not number is not an integer it should throw an error', () => {
-      assert.throws(() => primeFactors(1.1), /This number is not an integer!/);
-    });
+    assert.throws(() => primeFactors(1.1), /This number is not an integer!/);
+  });
 });
