5abd5f1dd71b948ac0e4849ee6d29a9341b52d0e
EddyeBoy27 <lipe0503@gmail.com>
2020-01-30 04:29:22 -0200

    Finalizado Last

--- tests/last.spec.js
+++ tests/last.spec.js
@@ -5,3 +5,15 @@ const last = require('../src/last');
 // Se a função receber uma lista, deve retornar o último elemento
 // Se a função receber uma string, deve retornar o último caractere
 // Se a função receber vários parâmetros, deve retornar o último parâmetro
+
+describe('#last', () => {
+  it('expect last letter of Felipe, e', () => {
+    expect(last('Felipe')).toEqual('e');
+  });
+  it('expect last array indice Miguel', () => {
+    expect(last(['Felipe', 'Max', 'Miguel'])).toEqual('Miguel');
+  });
+  it('expect last param', () => {
+    expect(last('Felipe', 12, true)).toEqual(true);
+  });
+});
