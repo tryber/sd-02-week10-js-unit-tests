8089f969e091fb26840378747ca1fa8e80dfe5e9
Ishidor <kevinmendessilva@hotmail.com>
2020-01-30 13:29:38 -0300

    Steps 9 & 10 done

--- tests/printBinary.spec.js
+++ tests/printBinary.spec.js
@@ -7,10 +7,18 @@ const printBinary = require('../src/printBinary');
 
 describe('#printBinary', () => {
   it('should print binary numbers until 1', () => {
-    expect(printBinary(1)).toBe(['0', '1']);
+    expect(printBinary(1)).toStrictEqual(['0', '1']);
   });
-
   it('should print binary numbers until 2', () => {
-    expect(printBinary(2)).toBe(['00', '01', '10', '11']);
+    expect(printBinary(2)).toStrictEqual(['00', '01', '10', '11']);
+  });
+  it('should print binary numbers until 3', () => {
+    expect(printBinary(3)).toStrictEqual(['000', '001', '010', '011', '100', '101', '110', '111']);
+  });
+  it('empty array shouldd be returned because of a string in param', () => {
+    expect(printBinary('a')).toStrictEqual([]);
+  });
+  it('empty array shouldd be returned because of a 0 in param', () => {
+    expect(printBinary(0)).toStrictEqual([]);
   });
 });
