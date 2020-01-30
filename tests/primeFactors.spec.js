4c3f15d6b4a58431486de706087c161f462e08cd
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2020-01-30 11:19:35 -0200

    Code Climate

--- tests/primeFactors.spec.js
+++ tests/primeFactors.spec.js
@@ -4,8 +4,6 @@ const assert = require('assert');
 // A função recebe um inteiro e retorna a lista com os fatores primos desse número
 // Se o parâmetro recebido for um valor não inteiro um erro deve ser lançado
 
-
-
 describe('#primeFactors', () => {
   it('tests', () => {
     const expected = primeFactors(40);
