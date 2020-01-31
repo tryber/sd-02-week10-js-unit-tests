a44b945a48329aa0dab950367cfedda792aa43a2
guicgs <etohlif@protonmail.com>
2020-01-31 18:05:21 -0300

    função printBinary concluída

--- src/printBinary.js
+++ src/printBinary.js
@@ -3,20 +3,20 @@
   e retorna uma lista com todos os números binários existentes com a quantidade ${number} de dígitos
  */
 
+ // solução Miguel comentada - raciocínio de análise combinatória
 function printBinary(number) {
-  // const lista = ['0', '1'];
-
-  // for (let i = 1; i < number; i += 1) {
-  //   let tamanhoLista = lista.length;
-
-  //   for (let j = 0; j < tamanhoLista; j += 1) {
-  //     lista.push(`${lista[j]}0`);
-  //     lista.push(`${lista[j]}1`);
-  //   }
-  // }
-  // console.log(lista)
-
-  // return lista.slice(lista.length - 2 ** number);
+  const binaries = ['0', '1']; // array de números que serão combinados
+  for (let i = 1; i < number; i += 1) { // loop para redefinir o valor do tamanho da lista
+    const binariesLength = binaries.length;
+    for (let j = 0; j < binariesLength; j += 1) {
+      // loop nos elementos da lista que são inseridos acompanhados de 0 e 1
+      binaries.push(`${binaries[j]}0`);
+      binaries.push(`${binaries[j]}1`);
+    }
+  }
+  // return binaries.splice(binaries.length - (2 ** number));
+  return binaries.filter(element => element.length === number);
+  // filtra somente os números com o número de dígitos desejado
 }
 
 module.exports = printBinary;
