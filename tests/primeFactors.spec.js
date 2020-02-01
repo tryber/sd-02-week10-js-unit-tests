9e49f94f6d22ed65c2566b9cc0bb3c02f48c6acc
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-02-01 12:41:50 -0300

    Adicionado test para evitar falha do jest (arquivo só tinha asserts) - primeFactors

--- tests/primeFactors.spec.js
+++ tests/primeFactors.spec.js
@@ -4,6 +4,11 @@ const assert = require('assert');
 // A função recebe um inteiro e retorna a lista com os fatores primos desse número
 // Se o parâmetro recebido for um valor não inteiro um erro deve ser lançado
 
+// Um único test só pro Jest não falhar aqui
+test('is a function', () => {
+  expect(typeof primeFactors).toBe('function');
+});
+
 assert.strictEqual(typeof primeFactors, 'function');
 assert.deepStrictEqual(primeFactors(10), [2, 5]);
 assert.deepStrictEqual(primeFactors(187), [11, 17]);
