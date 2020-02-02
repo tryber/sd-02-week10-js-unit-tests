d57f495f5d5a2b4ba292373df68899982f6b2f40
EddyeBoy27 <lipe0503@gmail.com>
2020-02-02 00:45:37 -0200

    Atualizado primeFactors pra utilizar biblioteca assert

--- tests/primeFactors.spec.js
+++ tests/primeFactors.spec.js
@@ -1,21 +1,9 @@
+const assert = require('assert');
 const primeFactors = require('../src/primeFactors');
-// const assert = require('assert');
 
 // A função recebe um inteiro e retorna a lista com os fatores desse número
 // Se o parâmetro recebido for um valor não inteiro um erro deve ser lançado
 
-// const expect = primeFactors(112);
+const expectWithPrimers = primeFactors(109200);
 
-// assert.deepStrictEqual(expect, [ 2, 2, 2, 2, 7 ], 'Deu ruim, tenta de novo');
-
-describe('#primeFactors', () => {
-  it('expect factoring 112 [2, 2, 2, 2, 7]', () => {
-    expect(primeFactors(112)).toEqual([2, 2, 2, 2, 7]);
-  });
-  it('expect factoring 109200 [2, 2, 2, 2, 3, 5, 5, 7, 13]', () => {
-    expect(primeFactors(109200)).toEqual([2, 2, 2, 2, 3, 5, 5, 7, 13]);
-  });
-  it('expect error message if dont send number param', () => {
-    expect(primeFactors(3.12)).toBe('3.12 is not a integer number');
-  });
-});
+assert.deepStrictEqual(expectWithPrimers, [ 2, 2, 2, 2, 3, 5, 5, 7, 13 ], 'Deu ruim, tenta de novo');
