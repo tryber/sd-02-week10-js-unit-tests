3337931c28670977881b0398c995f266861411c0
EddyeBoy27 <lipe0503@gmail.com>
2020-02-02 18:16:07 -0200

    Finalizado primeiro e segundo teste da caller

--- tests/caller.spec.js
+++ tests/caller.spec.js
@@ -5,3 +5,15 @@ const caller = require('../src/caller');
 // O segundo e terceiro parâmetros podem ser de qualquer tipo
 // A função deve executar a função (primeiro parâmetro) com os outros parâmetros recebidos
 // e retornar o resultado
+
+const mockedFunction = jest.fn((a, b) => a + b);
+
+describe('#caller', () => {
+  test('If caller is a function', () => {
+    expect(typeof caller).toEqual('function');
+  });
+
+  test('If caller receive 3 params', () => {
+    expect(caller.length).toBe(3);
+  });
+});
