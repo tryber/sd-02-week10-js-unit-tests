a89c82228945f7ca51bc168ae4e3ca86088e67d5
Giorgia <giorgialeoniac@gmail.com>
2020-02-02 22:26:59 -0300

    Resolvendo alguns testes que faltam

--- tests/greeting.spec.js
+++ tests/greeting.spec.js
@@ -3,3 +3,9 @@ const greeting = require('../src/greeting');
 // A função deve receber uma string ${param} e retornar a mensagem 'Hello ${param}!'
 // Se nenhum parâmetro for passado um erro deve ser lançado
 // A mensagem do erro deve ser 'You must send something to greet!'
+
+
+test('Retornar greeting', () => {
+    expect(greeting('Fulano')).toBe('Hello Fulano!');
+    expect(() => greeting()).toThrow('You must send something to greet!');
+});
