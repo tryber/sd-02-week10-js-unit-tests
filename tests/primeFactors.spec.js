51a1877e0f69ca99049a096dd01db8d842db3490
phtf92 <phtf92@gmail.com>
2020-02-04 19:07:46 -0300

    fixando prime factor

--- tests/primeFactors.spec.js
+++ tests/primeFactors.spec.js
@@ -3,12 +3,13 @@ const assert = require('assert');
 
 // A função recebe um inteiro e retorna a lista com os fatores primos desse número
 // Se o parâmetro recebido for um valor não inteiro um erro deve ser lançado
-
-assert.deepEqual(primeFactors(4), [2, 2]);
-assert.deepEqual(primeFactors(10), [2, 5]);
-assert.deepEqual(primeFactors(7), [7]);
-assert.deepEqual(primeFactors(9), [3, 3]);
-assert.deepEqual(primeFactors(24), [2, 2, 2, 3]);
-assert.throws(() => {
-  primeFactors(4.5);
-});
+test('teste primos', () => {
+  assert.deepEqual(primeFactors(4), [2, 2]);
+  assert.deepEqual(primeFactors(10), [2, 5]);
+  assert.deepEqual(primeFactors(7), [7]);
+  assert.deepEqual(primeFactors(9), [3, 3]);
+  assert.deepEqual(primeFactors(24), [2, 2, 2, 3]);
+  assert.throws(() => {
+    primeFactors(4.5);
+  });
+})
